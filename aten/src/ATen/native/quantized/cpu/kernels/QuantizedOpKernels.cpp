9cdfd59095636f2c185f094892197ba6b4a6d5ae
Peter Bell <peterbell10@live.co.uk>
2022-01-22 18:56:51 +0000

    Remove native_functions.yaml dependency from Sorting.cpp
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66980
    
    Differential Revision: [D31856101](https://our.internmc.facebook.com/intern/diff/D31856101/)
    ghstack-source-id: 6fdfb56e4ca16884492a056075c0e502ff382a2a

--- aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
+++ aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
@@ -3,7 +3,7 @@
 #include <ATen/core/List.h>
 #include <ATen/Dispatch.h>
 #include <ATen/Parallel.h>
-#include <ATen/native/SortingUtils.h>
+#include <ATen/native/TopKImpl.h>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/UpSample.h>
 #include <ATen/native/cpu/Loops.h>
