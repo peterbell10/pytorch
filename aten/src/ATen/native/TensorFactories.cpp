bac2296c6b25631648681cefdb1b04f8b8157489
Peter Bell <peterbell10@live.co.uk>
2022-02-08 20:44:55 +0000

    [Testing] Add new method operator

--- aten/src/ATen/native/TensorFactories.cpp
+++ aten/src/ATen/native/TensorFactories.cpp
@@ -31,6 +31,10 @@ Tensor build_test_operator(const Tensor &self) {
   return self;
 }
 
+Tensor build_test_method_operator(const Tensor &self) {
+  return self;
+}
+
 namespace {
 void window_function_checks(
     const char* function_name,
