af8757df7f451ada403699fc3945b35cdc46f551
Peter Bell <peterbell10@live.co.uk>
2022-11-06 23:20:37 +0000

    Fix segfault in has_torch_function

--- torch/csrc/utils/disable_torch_function.cpp
+++ torch/csrc/utils/disable_torch_function.cpp
@@ -263,6 +263,9 @@ PyObject* THPModule_has_torch_function(PyObject*, PyObject* arg) {
   } else {
     auto args = py::reinterpret_steal<py::object>(
         PySequence_Fast(arg, "expected a sequence"));
+    if (!args) {
+      throw py::error_already_set();
+    }
     result = sequence_has_torch_function(args.ptr());
   }
 
