799b42b98838392afdd24f72ee244befd2e7d100
Peter Bell <peterbell10@live.co.uk>
2023-05-02 13:38:31 +0100

    Fix AT_USE_JITERATOR checks
    `AT_USE_JITERATOR` evaluates to false when the definition isn't
    included, so these files were not using jiterator at all.
    
    ghstack-source-id: be1e006df9120ab108330eed79a54894a7ab80cd
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100464

--- aten/src/ATen/native/cuda/UnaryGeometricAcoshKernel.cu
+++ aten/src/ATen/native/cuda/UnaryGeometricAcoshKernel.cu
@@ -5,20 +5,21 @@
 #include <ATen/native/DispatchStub.h>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/UnaryOps.h>
+#include <ATen/native/cuda/JitLoops.cuh>
 #include <ATen/native/cuda/Loops.cuh>
 #include <ATen/native/cuda/Math.cuh>
 #include <limits>
 
 namespace at::native {
 
-CONSTEXPR_EXCEPT_WIN_CUDA char acosh_name[] = "acosh";
+CONSTEXPR_EXCEPT_WIN_CUDA char acosh_name[] = "acosh_impl";
 void acosh_kernel_cuda(TensorIteratorBase& iter) {
   auto common_dtype = iter.common_dtype();
   if(at::isComplexType(common_dtype)) {
-#if AT_USE_JITERATOR
+#if AT_USE_JITERATOR()
   static const auto acosh_string = jiterator_stringify(
     template <typename T>
-    T acosh(T a) {
+    T acosh_impl(T a) {
         return std::acosh(a);
     }
   );
